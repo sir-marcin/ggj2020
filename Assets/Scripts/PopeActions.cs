64d1dad39048c9a6c3036d10b638ea2669052729
Marcin Murawski <mrc.murawski@gmail.com>
2020-02-01 14:14:26 +0100

    selection gizmos

--- Assets/Scripts/PopeActions.cs
+++ Assets/Scripts/PopeActions.cs
@@ -3,20 +3,55 @@ using UnityEngine;
 
 namespace Pope
 {
+    [RequireComponent(typeof(Camera))]
     public class PopeActions : MonoBehaviour
     {
-        [SerializeField] Camera camera;
+        const string pilgrimsTag = "Pilgrims";
+        
+        [SerializeField] GameObject lightBeam;
+        [SerializeField] Transform raycastDirectionObject;
+        
+        Transform beam;
+        Camera camera;
+        Vector3 rayDirection;
         
         const float raycastMaxDistance = 360f;
+
+        void Awake()
+        {
+            camera = GetComponent<Camera>();
+            rayDirection = camera.transform.forward;
+        }
+
+        void Start()
+        {
+            beam = Instantiate(lightBeam).transform;
+        }
         
         void Update()
         {
-            if (Input.GetKey(KeyCode.Space))
-            {
-                Bless();
-            }
+            Ray();
         }
 
+        void Ray()
+        {
+            rayDirection = raycastDirectionObject.forward;
+
+            Vector3 rayStart = camera.ViewportToWorldPoint(Vector3.zero);
+            
+            if (Physics.Raycast(rayStart, rayDirection, out var hit, raycastMaxDistance))
+            {
+                if (hit.collider.CompareTag(pilgrimsTag))
+                {
+                    beam.position = hit.collider.transform.position;
+                }
+                else
+                {
+                    beam.position = hit.point;
+                }
+            }
+        }
+        
         void Bless()
         {
             Vector3 rayDirection = camera.transform.forward;
