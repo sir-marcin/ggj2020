19214702c4b1a2b108be34239f9ce2a4d7ed7083
Marek Polasik <marx432@gmail.com>
2020-02-01 22:17:53 +0100

    feather pigeon pwn

---
+++ Assets/ParticleSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSpawner : MonoBehaviour
+{
+    public static ParticleSpawner Instance;
+
+    ParticleSystem[] particleSystems;
+    int index;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particleSystems = GetComponentsInChildren<ParticleSystem>(true);
+    }
+
+    public void Spawn(Vector3 pos)
+    {
+        if (index >= particleSystems.Length)
+        {
+            index = 0;
+
+        }
+        particleSystems[index].transform.position = pos;
+        particleSystems[index].gameObject.SetActive(false);
+        particleSystems[index].gameObject.SetActive(true);
+        index++;
+
+    }
+}
---
+++ Assets/Pigeon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pigeon : MonoBehaviour
+{
+    const string pigeonTag = "Pigeon";
+
+    private void OnTriggerEnter(Collider other) {
+        if (!other.CompareTag(pigeonTag)){
+            ParticleSpawner.Instance.Spawn(transform.position);
+            Destroy(gameObject);
+        }
+    }
+}
